5a5c5a60bbbd585081132bdb5a1497a1dfad722b
PhantomBantam <lucnguyen1416@gmail.com>
2020-08-19 16:11:39 -0700

    created basic user routes, set up ejs

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+
+
 app.get('/', (req, res)=>{
   res.send('webscraper');
 });
+
+const users = require('./routes/users');
+app.use('/users', users)
+
 
 
 app.listen(PORT, ()=>console.log('SERVER RUNNING AT PORT ' + PORT));
